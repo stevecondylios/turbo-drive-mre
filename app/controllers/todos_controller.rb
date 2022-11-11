95a29f3637a746583b5535117bd1973fe1f3a21b
Steve Condylios <steve.condylios@gmail.com>
2022-11-11 16:02:47 +0200

    Use turbo stream to prepend to a list of todos after a form submission

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -25,11 +25,10 @@ class TodosController < ApplicationController
 
     respond_to do |format|
       if @todo.save
+        format.turbo_stream
         format.html { redirect_to todo_url(@todo), notice: "Todo was successfully created." }
-        format.json { render :show, status: :created, location: @todo }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @todo.errors, status: :unprocessable_entity }
       end
     end
   end
